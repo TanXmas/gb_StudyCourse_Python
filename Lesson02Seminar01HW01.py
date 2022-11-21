c943643e943bf6c0e6be9d54c2075104657b35d4
TXmas <nushaxmas@gmail.com>
2022-11-21 21:52:13 +0300

    init

---
+++ Lesson02Seminar01HW01.py
@@ -0,0 +1,15 @@
+ans = False
+while not ans:
+    print('Введите номер дня недели: ', end='')
+    try:
+        n = int(input())
+    except ValueError:
+        n = 0
+    if 1 <= n <= 5:
+        ans = True
+        print('нет')
+    elif 6 <= n <= 7:
+        ans = True
+        print('да')
+    else:
+        ans = False
