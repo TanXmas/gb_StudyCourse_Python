e9f1549c4ea0fbc7dad884e5438414e70817499b
TXmas <nushaxmas@gmail.com>
2022-11-22 11:24:46 +0300

    L02S01HW_Task5

--- Lesson02Seminar01HW05.py
+++ Lesson02Seminar01HW05.py
@@ -1,16 +1,13 @@
-#Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
-def get_point(n=0, axis='x'):
-    while not n:
-        print(f'Введите координату {axis}: ', end='')
-        try:
-            n = int(input())
-            if n != 0:
-                return n
-            else:
-                n = 0
-        except ValueError:
-            n = 0
+# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
+def get_point(axis):
+    print(f'Введите координату {axis}: ', end='')
+    n = input()
+    try:
+        return int(n)
+    except ValueError:
+        n = 0
+        return n + get_point(axis)
 
-x1, y1 = get_point(axis='x1'), get_point(axis='y1')
-x2, y2 = get_point(axis='x2'), get_point(axis='y2')
+x1, y1 = get_point('x1'), get_point('y1')
+x2, y2 = get_point('x2'), get_point('y2')
 print(f'{((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5:.02f}')
