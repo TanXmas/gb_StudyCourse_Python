cb04996a3bafd53285366a6a18e7cb590ad0eb04
TXmas <nushaxmas@gmail.com>
2022-11-22 16:15:03 +0300

    L02S01HW_Task8

--- Lesson02Seminar01HW08.py
+++ Lesson02Seminar01HW08.py
@@ -1,14 +1,14 @@
 # Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
 # Для решения используйте цикл while и арифметические операции.
 def get_num():
-    err = True
-    while err:
-        print(f'Введите целое положительное число: ', end='')
-        try:
-            n = int(input())
-            if n >= 0: return n
-        except ValueError:
-            err = True
+    print(f'Введите целое положительное число: ', end='')
+    try:
+        n = int(input())
+    except ValueError:
+        n = 0
+    if n > 0: return n
+    return get_num()
+
 
 n = get_num()
 print(f'Самая большая цифра в числе {n} = ', end='')
