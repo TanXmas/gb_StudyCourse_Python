a219a4b1cbd43d626e74e19378b807c8e0b2c1e5
TXmas <nushaxmas@gmail.com>
2022-11-22 16:14:41 +0300

    L02S01HW_Task4

--- Lesson02Seminar01HW04.py
+++ Lesson02Seminar01HW04.py
@@ -1,20 +1,20 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
-ans = False
-while not ans:
+def get_num():
     print('Введите номер координатной четверти: ', end='')
     try:
         n = int(input())
     except ValueError:
         n = 0
-    if n == 1:
-        ans = True
-        print('x > 0, y > 0')
-    elif n == 2:
-        ans = True
-        print('x < 0, y > 0')
-    elif n == 3:
-        ans = True
-        print('x < 0, y < 0')
-    elif n == 4:
-        ans = True
-        print('x > 0, y < 0')
+    if 1 <= n <= 4: return n
+    return get_num()
+
+
+n = get_num()
+if n == 1:
+    print('x > 0, y > 0')
+elif n == 2:
+    print('x < 0, y > 0')
+elif n == 3:
+    print('x < 0, y < 0')
+else:
+    print('x > 0, y < 0')
