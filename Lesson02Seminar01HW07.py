3d6a921b411cb2f41631ac89726e52e142d5a9e7
TXmas <nushaxmas@gmail.com>
2022-11-22 16:14:58 +0300

    L02S01HW_Task7

--- Lesson02Seminar01HW07.py
+++ Lesson02Seminar01HW07.py
@@ -1,12 +1,13 @@
 # Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-def get_num(n=0):
-    while not n:
-        print(f'Введите натуральное число: ', end='')
-        try:
-            n = int(input())
-            if n > 0: return str(n)
-        except ValueError:
-            n = 0
+def get_num():
+    print(f'Введите натуральное число: ', end='')
+    try:
+        n = int(input())
+    except ValueError:
+        n = 0
+    if n > 0: return str(n)
+    return get_num()
+
 
 n = get_num()
 print(n, ' + ', n*2, ' + ', n*3, ' = ', int(n) + int(n * 2) + int(n * 3), sep='')
