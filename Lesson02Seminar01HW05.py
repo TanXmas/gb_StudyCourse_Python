6554bc8228af358e36dc6b65e27b7066a9b16a0d
TXmas <nushaxmas@gmail.com>
2022-11-22 16:14:47 +0300

    L02S01HW_Task5

--- Lesson02Seminar01HW05.py
+++ Lesson02Seminar01HW05.py
@@ -5,9 +5,12 @@ def get_point(axis):
     try:
         return int(n)
     except ValueError:
-        n = 0
-        return n + get_point(axis)
+        try:
+            return float(n)
+        except ValueError:
+            return get_point(axis)
+
 
 x1, y1 = get_point('x1'), get_point('y1')
 x2, y2 = get_point('x2'), get_point('y2')
-print(f'{((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5:.02f}')
+print(f'Расстояние между точками ({x1}, {y1}) и ({x2}, {y2}) = {((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5:.02f}')
