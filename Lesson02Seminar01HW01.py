8f1c1b4f828588d31be9dbf6b71a4d8952c0bd72
TXmas <nushaxmas@gmail.com>
2022-11-22 16:14:25 +0300

    L02S01HW_Task1

--- Lesson02Seminar01HW01.py
+++ Lesson02Seminar01HW01.py
@@ -1,15 +1,18 @@
 # Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-ans = False
-while not ans:
+def get_num():
     print('Введите номер дня недели: ', end='')
     try:
         n = int(input())
     except ValueError:
         n = 0
-    if 1 <= n <= 5:
-        ans = True
-        print('нет')
-    elif 6 <= n <= 7:
-        ans = True
-        print('да')
+    if 1 <= n <= 7: return n
+    return get_num()
 
+
+n = get_num()
+if 1 <= n <= 5:
+    ans = True
+    print('нет')
+elif 6 <= n <= 7:
+    ans = True
+    print('да')
