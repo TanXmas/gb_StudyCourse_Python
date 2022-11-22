f17c506959b0128dd3cbf590b63911be7f5b3f33
TXmas <nushaxmas@gmail.com>
2022-11-22 16:14:31 +0300

    L02S01HW_Task2

--- Lesson02Seminar01HW02.py
+++ Lesson02Seminar01HW02.py
@@ -1,7 +1,8 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
-
-print('{:^3}|{:^3}|{:^3}|{:^10}|{:^10}'.format('x', 'y', 'z', '¬(X⋁Y⋁Z)', '¬X⋀¬Y⋀¬Z'))
+print('{:^5}|{:^5}|{:^5}|{:^10}|{:^10}'.format('x', 'y', 'z', '¬(X⋁Y⋁Z)', '¬X⋀¬Y⋀¬Z'))
 for x in (0, 1):
     for y in (0, 1):
         for z in (0, 1):
-            print('{:^3}|{:^3}|{:^3}|{:^10}|{:^10}'.format(x, y, z, int(not(x or y or z)), int(not(x) and not(y) and not(z))))
+            print('{:^5}|{:^5}|{:^5}|{:^10}|{:^10}'.format(x, y, z,
+                                                           int(not(x or y or z)),
+                                                           int(not(x) and not(y) and not(z))))
